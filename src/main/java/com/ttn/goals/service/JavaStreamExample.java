eb8a99ea8d71dede86287dbc8e944e41d77839ee
Prakhar Sharma <prakhar.sharma@tothenew.com>
2019-10-22 00:35:32 +0530

    Stream Method

--- src/main/java/com/ttn/goals/service/JavaStreamExample.java
+++ src/main/java/com/ttn/goals/service/JavaStreamExample.java
@@ -4,6 +4,7 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -33,8 +34,27 @@ public class JavaStreamExample {
 
         System.out.println(stringList.stream().anyMatch(s -> s.startsWith("p")));
 
-        List<String> s1 =stringList.stream().filter(s -> s.startsWith("p")).collect(Collectors.toList());
-        System.out.println(s1);
+        stringList.stream().filter(s -> s.startsWith("I")).forEach(System.out::println);
+
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("Amitabh");
+        list2.add("Shekhar");
+        list2.add("Aman");
+        list2.add("Rahul");
+        list2.add("Shahrukh");
+        list2.add("Salman");
+        list2.add("Yana");
+        list2.add("Lokesh");
+
+        list2.stream().filter(s -> s.startsWith("S")).forEach(System.out::println);
+        list2.stream().sorted().forEach(System.out::println);
+        System.out.println();
+        list2.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+
+        list2.stream().filter(s -> s.startsWith("A")).map(String::toUpperCase).forEach(System.out::println);
+        System.out.println(stringList.stream().filter(s -> s.startsWith("A")).map(String::toUpperCase).reduce((s, s2) ->s +" "+s2 ).get());
+        System.out.println("Count :: "+stringList.stream().filter(s -> s.startsWith("A")).count());
 
 
 
